e293edaadb4653e0706b2da4708185a36a853e0f
Matthew Thomas <mzthomas96@gmail.com>
2021-01-14 20:16:38 -0500

    commit tree 97274df7a4cae964848a87cb8f0cfd17a6b8ab2f

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,10 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+@@brands = []
   def initialize(brand)
     @brand = brand
+    @@brands << brand
   end
 
   def cobble
